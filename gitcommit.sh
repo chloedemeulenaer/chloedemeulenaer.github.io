b8382c90b32b00eaeb935a139beda6b8d0a811d6
Chloé de Meulenaer <chloedm@protonmail.com>
2024-10-21 18:20:19 +0100

    Added robots to avoid indexing

---
+++ gitcommit.sh
@@ -0,0 +1,9 @@
+# Pour commit les changes 
+# GIT TOKEN: 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
+
+git clone https://github.com/chloedemeulenaer/chloedemeulenaer.github.io.git
+cd /Users/DEMEULEC/Documents/6 Recherche/Resources/Site/chloedemeulenaer.github.io
+
+git add robots.txt
+git commit -m "Renamed HTML file to index" 
+git push origin main 
