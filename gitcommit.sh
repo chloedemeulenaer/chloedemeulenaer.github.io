bc2238f6f3fb73cd0cbc7c33470ae12b86185594
Chloé de Meulenaer <chloedm@protonmail.com>
2024-10-21 18:22:30 +0100

    Blah

--- gitcommit.sh
+++
@@ -1,9 +0,0 @@
-# Pour commit les changes 
-# GIT TOKEN: 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
-
-git clone https://github.com/chloedemeulenaer/chloedemeulenaer.github.io.git
-cd "/Users/DEMEULEC/Documents/6 Recherche/Resources/Site/chloedemeulenaer.github.io"
-
-git add .
-git commit -m "Blah" 
-git push origin main 
--- tempCodeRunnerFile.sh
+++
@@ -1,5 +0,0 @@
-cd "/Users/DEMEULEC/Documents/6 Recherche/Resources/Site/chloedemeulenaer.github.io"
-
-git add .
-git commit -m "Blah" 
-git push origin main 
